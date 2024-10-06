3d446e582a77ad6a48ae5bca5541290428b8bd20
Abhimanyu-Developer123 <athivenkadesh@gmail.com>
2024-10-06 08:37:48 +0530

    basic program

--- basic/hello.py
+++ basic/hello.py
@@ -1,1 +1,22 @@
 print("hello")
+
+
+"""
+C:\githubprogram\pythonprogram>git commit -m "basic program"
+[main (root-commit) 31848e8] basic program
+ 1 file changed, 1 insertion(+)
+ create mode 100644 basic/hello.py
+
+C:\githubprogram\pythonprogram>git  add .
+
+C:\githubprogram\pythonprogram>git push
+info: please complete authentication in your browser...
+Enumerating objects: 4, done.
+Counting objects: 100% (4/4), done.
+Writing objects: 100% (4/4), 280 bytes | 140.00 KiB/s, done.
+Total 4 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+To https://github.com/Abhimanyu-Developer123/pythonprogram.git
+ * [new branch]      main -> main
+
+
+"""
