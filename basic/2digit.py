bbee7f493cb1b39c1e5f7ea793698ea45eea651f
Abhimanyu-Developer123 <athivenkadesh@gmail.com>
2025-01-10 08:50:30 +0530

    basic program

--- basic/2digit.py
+++ basic/2digit.py
@@ -3,6 +3,7 @@ print("enter a number ")
 no=int(input())
 if no>9 and no<100:
     print("2 digit number ")
+elif no>=100:
+	print("3 or more than 3 digit number")
 else:
 	print("1 digit number")
-	
